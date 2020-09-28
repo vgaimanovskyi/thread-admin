d2345f868e0975e21ca18c7b8e9ed2fd0ca87e95
vgaimanovskyi <vgaymanovskiy@gmail.com>
2020-09-28 12:23:20 +0300

    changed project name

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thread-admin/precache-manifest.ece0edc376040684810e4c2f6e66db81.js"
+  "/thread-admin/precache-manifest.dce28dab7893095ac0ce273db0e53f11.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "thread-admin"});
+workbox.core.setCacheNameDetails({prefix: "vlad-admin"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
